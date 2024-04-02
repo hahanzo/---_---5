777d3a6b0c1db56ae6b5ad8f793a973318b71336
Oleg <olegdrobit16@gmail.com>
2024-04-03 02:14:48 +0300

    gz

---
+++ server.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	// Встановлення маршрутів для відслідковування запитів
+	http.HandleFunc("/", serveIndex)
+	http.HandleFunc("/City.json", serveJSON)
+
+	// Запуск веб-сервера на порту 8000
+	fmt.Println("Server is running on http://localhost:8000")
+	http.ListenAndServe(":8000", nil)
+}
+
+// Функція для обробки запитів до головної сторінки
+func serveIndex(w http.ResponseWriter, r *http.Request) {
+	// Відправляємо відповідь клієнту
+	http.ServeFile(w, r, "newIndex.html")
+}
+
+// Функція для обробки запитів до файлу City.json
+func serveJSON(w http.ResponseWriter, r *http.Request) {
+	// Відправляємо відповідь клієнту
+	http.ServeFile(w, r, "City.json")
+}
